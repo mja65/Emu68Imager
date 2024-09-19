d6554e59b6f49cd422b097d62a4944f417f050be
mja65 <supremeturnip@hotmail.com>
2024-09-20 07:16:52 +1000

    Updated cmd file to include CD to path

--- PistormImagerGui.cmd
+++ PistormImagerGui.cmd
@@ -1,1 +1,2 @@
+cd "%~dp0"
 powershell.exe -Command "& {$wd = Get-Location; Start-Process powershell.exe -Verb RunAs -ArgumentList \"-ExecutionPolicy ByPass -NoExit -Command Set-Location '$wd'; .\Script\PistormImagerGUI.ps1\"}"
