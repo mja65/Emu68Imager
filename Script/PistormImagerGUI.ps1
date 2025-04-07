62599ce0adb598d4a64028b6433a1ddbadf45905
mja65 <supremeturnip@hotmail.com>
2025-04-08 09:12:48 +1000

    Update PistormImagerGUI.ps1
    Version bump to 1.0.5

--- Script/PistormImagerGUI.ps1
+++ Script/PistormImagerGUI.ps1
@@ -1,5 +1,5 @@
 <#PSScriptInfo
-.VERSION 1.0.4.3
+.VERSION 1.0.5
 .GUID 73d9401c-ab81-4be5-a2e5-9fc0834be0fc
 .AUTHOR SupremeTurnip
 .COMPANYNAME
@@ -20,7 +20,7 @@
 Script for Emu68Imager 
 #> 
 
-$Script:Version = '1.0.4.3'
+$Script:Version = '1.0.5'
 
 ####################################################################### Add GUI Types ################################################################################################################
 
