eb53097d50e41ea37f1d0ac28dbd38cfe9237d7a
mja65 <supremeturnip@hotmail.com>
2025-04-08 09:14:44 +1000

    Update PistormImagerGUI.ps1
    Added function Write-AmigaInfoType to allow changing of .info file types where not set correctly

--- Script/PistormImagerGUI.ps1
+++ Script/PistormImagerGUI.ps1
@@ -2236,6 +2236,66 @@ function Write-AmigaTooltypes {
         return $true
     }        
 }
+
+function Write-AmigaInfoType {
+    param (
+        $HSTAmigaPathtouse,
+        $TempFoldertouse,
+        $IconPath,
+        $TypetoSet
+
+    )
+
+    # $HSTAmigaPathtouse = 'E:\Emu68Imager\Working Folder\Programs\HST-Amiga\hst.amiga.exe'
+    # $IconPath = 'E:\Emulators\Amiga Files\3.9\hd.info'
+    # $TypetoSet = 'Disk'
+
+    if ($TypetoSet -eq 'Disk'){
+        $HSTParameter = '1'
+    }
+    elseif ($TypetoSet -eq 'Drawer'){
+        $HSTParameter = '2'
+    }
+    elseif ($TypetoSet -eq 'Tool'){
+        $HSTParameter = '3'
+    }
+    elseif ($TypetoSet -eq 'Project'){
+        $HSTParameter = '4'
+    }
+    elseif ($TypetoSet -eq 'Garbage'){
+        $HSTParameter = '5'
+    }
+    elseif ($TypetoSet -eq 'Device'){
+        $HSTParameter = '6'
+    }
+    elseif ($TypetoSet -eq 'Kick'){
+        $HSTParameter = '7'
+    }
+    elseif ($TypetoSet -eq 'AppIcon'){
+        $HSTParameter = '8'
+    }
+
+    $Logoutput = ($TempFoldertouse+'LogOutputTemp.txt')
+
+    & $HSTAmigaPathtouse icon update $IconPath -t  $HSTParameter >$Logoutput
+    $CheckforError = Get-Content ($Logoutput)
+    $ErrorCount=0
+    foreach ($ErrorLine in $CheckforError){
+        if ($ErrorLine -match " ERR]"){
+            $ErrorCount += 1
+            Write-ErrorMessage -Message ('Error in HST-Amiga: '+$ErrorLine)           
+        }
+    }
+    if ($ErrorCount -ge 1){
+        $null=Remove-Item ($Logoutput) -Force
+        return $false    
+    }
+    else{
+        return $true
+    }     
+
+}
+
 function Write-AmigaIconPostition {
     param (
         $HSTAmigaPathtouse,
